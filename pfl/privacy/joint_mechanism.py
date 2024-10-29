92eff6a858574f54ac2b09df1cd3b985897edf38
Jonny Scott <jonnyascott01@gmail.com>
2024-10-29 11:49:05 +0100

    updated typing

--- pfl/privacy/joint_mechanism.py
+++ pfl/privacy/joint_mechanism.py
@@ -17,39 +17,28 @@ from .privacy_mechanism import CentrallyApplicablePrivacyMechanism, PrivacyMecha
 from .privacy_snr import SNRMetric
 
 
-def check_if_partition(full_list: Set[str], partition: List[Set[str]]):
+def check_if_partition(full_set: Set[str], partition: List[Set[str]]):
     """
     Checks if each element of full_list appears in exactly one of
-    the lists in partition
+    the sets in partition
     """
-    full_set = set(full_list)
-    size_of_partition = 0
-    for l in partition:
-        size_of_partition += len(l)
-        for s in l:
-            if s not in full_set:
-                # partition contains an element not in full_list
-                return False
+    partition_union: Set = set()
+    partition_size = 0
+    for s in partition:
+        partition_union = partition_union.union(s)
+        partition_size += len(s)
 
-    if not size_of_partition == len(full_set):
-        # partition is missing an element
-        return False
-
-    return True
-
-
-
-
+    return (partition_union == full_set) and (partition_size == len(full_set))
 
 
 class JointMechanism(CentrallyApplicablePrivacyMechanism):
     """
     Constructs a new CentrallyApplicablePrivacyMechanism from existing ones.
     Each existing mechanism is applied to a disjoint subset of the client
-    statistic keys. As such JointMechanism can only be applied to client
+    statistics keys. As such JointMechanism can only be applied to client
     statistics of type MappedVectorStatistics.
 
-    :param mechanisms_to_keys:
+    :param mechanisms_and_keys:
         Dictionary in which each key is a name of a mechanism and each value
         is a tuple consisting of the corresponding CentrallyApplicablePrivacyMechanism
         and a list of keys specifying which portion of the user training statistics that
@@ -57,61 +46,87 @@ class JointMechanism(CentrallyApplicablePrivacyMechanism):
         distinct for the purpose of naming the corresponding Metrics.
 
     """
-    def __init__(self, mechanisms_to_keys: Dict[str, Tuple[CentrallyApplicablePrivacyMechanism, List[str]]]):
-        if len(set(mechanisms_to_keys.keys())) < len(mechanisms_to_keys.keys()):
+
+    def __init__(
+        self,
+        mechanisms_and_keys: Dict[str,
+                                  Tuple[CentrallyApplicablePrivacyMechanism,
+                                        List[str]]]):
+        if len(set(mechanisms_and_keys.keys())) < len(
+                mechanisms_and_keys.keys()):
             raise ValueError('Mechanism names must be unique.')
-        self.mechanisms_to_keys = mechanisms_to_keys
+        self.mechanisms_and_keys = mechanisms_and_keys
 
     def constrain_sensitivity(
             self,
-            statistics: MappedVectorStatistics,
+            statistics: TrainingStatistics,
             name_formatting_fn=lambda n: StringMetricName(n),
-            seed: Optional[int] = None) -> Tuple[MappedVectorStatistics, Metrics]:
+            seed: Optional[int] = None) -> Tuple[TrainingStatistics, Metrics]:
 
+        if not check_if_partition(
+                set(statistics.keys()),
+            [set(keys) for _, keys in self.mechanisms_and_keys.values()]):
+            raise ValueError(
+                'Mechanism keys do not form a partition of the client statistics keys.'
+            )
 
-        if not check_if_partition(list(statistics.keys()), [keys for _, keys in self.mechanisms_to_keys.values()]):
-            raise ValueError('Mechanism keys do not form a partition of the client statistics.')
+        if not isinstance(statistics, MappedVectorStatistics):
+            raise ValueError(
+                'Statistics must be of type MappedVectorStatistics.')
 
-        clipped_statistics = MappedVectorStatistics(weight=statistics.weight)
         metrics = Metrics()
-        for mechanism_name, (mechanism, statistics_keys) in self.mechanisms_to_keys.items():
+        for mechanism_name, (
+                mechanism,
+                statistics_keys) in self.mechanisms_and_keys.items():
+
             def mechanism_name_formatting_fn(n, prefix=mechanism_name):
                 return name_formatting_fn(f'{prefix}: {n}')
 
-            sub_statistics = MappedVectorStatistics()
+            sub_statistics: MappedVectorStatistics = MappedVectorStatistics()
             for key in statistics_keys:
                 sub_statistics[key] = statistics[key]
-            clipped_sub_statistics, sub_metrics = mechanism.constrain_sensitivity(sub_statistics,
-                                                                                  mechanism_name_formatting_fn,
-                                                                                  seed)
+            clipped_sub_statistics, sub_metrics = mechanism.constrain_sensitivity(
+                sub_statistics, mechanism_name_formatting_fn, seed)
             for key in statistics_keys:
-                clipped_statistics[key] = clipped_sub_statistics[key]
+                statistics[key] = clipped_sub_statistics[key]
             metrics = metrics | sub_metrics
 
-        return clipped_statistics, metrics
+        return statistics, metrics
 
     def add_noise(
             self,
-            statistics: MappedVectorStatistics,
+            statistics: TrainingStatistics,
             cohort_size: int,
             name_formatting_fn=lambda n: StringMetricName(n),
-            seed: Optional[int] = None) -> Tuple[MappedVectorStatistics, Metrics]:
+            seed: Optional[int] = None) -> Tuple[TrainingStatistics, Metrics]:
 
-        noised_statistics = MappedVectorStatistics(weight=statistics.weight)
+        if not check_if_partition(
+                set(statistics.keys()),
+            [set(keys) for _, keys in self.mechanisms_and_keys.values()]):
+            raise ValueError(
+                'Mechanism keys do not form a partition of the client statistics keys.'
+            )
+
+        if not isinstance(statistics, MappedVectorStatistics):
+            raise ValueError(
+                'Statistics must be of type MappedVectorStatistics.')
+
         metrics = Metrics()
-        for mechanism_name, (mechanism, statistics_keys) in self.mechanisms_to_keys.items():
+        for mechanism_name, (
+                mechanism,
+                statistics_keys) in self.mechanisms_and_keys.items():
+
             def mechanism_name_formatting_fn(n, prefix=mechanism_name):
                 return name_formatting_fn(f'{prefix}: {n}')
 
-            sub_statistics = MappedVectorStatistics()
+            sub_statistics: MappedVectorStatistics = MappedVectorStatistics()
             for key in statistics_keys:
                 sub_statistics[key] = statistics[key]
-            noised_sub_statistics, sub_metrics = mechanism.add_noise(sub_statistics,
-                                                                     cohort_size,
-                                                                     mechanism_name_formatting_fn,
-                                                                     seed)
+            noised_sub_statistics, sub_metrics = mechanism.add_noise(
+                sub_statistics, cohort_size, mechanism_name_formatting_fn,
+                seed)
             for key in statistics_keys:
-                noised_statistics[key] = noised_sub_statistics[key]
+                statistics[key] = noised_sub_statistics[key]
             metrics = metrics | sub_metrics
 
-        return noised_statistics, metrics
+        return statistics, metrics
